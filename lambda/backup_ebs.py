69a38033eda9f260dd34b45caa35eec651c96cf4
DHN Chandan <php.chandan@gmail.com>
2022-07-28 16:47:17 +0600

    feat: Extend ebs volume by given percentage

--- lambda/backup_ebs.py
+++ lambda/backup_ebs.py
@@ -1,4 +1,36 @@
+import time
+
 import boto3
+
+
+def get_ebs_volume_size(client, volume_id):
+    response = client.describe_volumes(VolumeIds=[volume_id])['Volumes'][0]['Size']
+    print('[volume_id] [size]', response)
+    return response
+
+
+def extend_volume(client, volume_id, new_size):
+    print(f'[extending] Going to extend volume {volume_id} to {new_size}G')
+    try:
+        client.modify_volume(
+            VolumeId=volume_id,
+            Size=new_size
+        )
+        return {"success": True, "message": f"Volume {volume_id} extended successfully to {new_size}G"}
+    except Exception as e:
+        print(f'[error] Unable to extend volume. Exception: {e}')
+        return {"success": False, "message": f"Unable to extend volume. Exception: {e}"}
+
+
+def wait_volume_modified(client, volume_id):
+    available_states = ["optimizing", "completed"]
+    state = ""
+    while state not in available_states:
+        state = client.describe_volumes_modifications(VolumeIds=[volume_id])['VolumesModifications'][0].get(
+            'ModificationState')
+        print(f'[state] Volume {volume_id} still not available, waiting...')
+        time.sleep(5)
+    return True
 
 
 def handler(event, context):
@@ -7,7 +39,9 @@ def handler(event, context):
 
     ec2 = boto3.client('ec2')
 
-    params = event["queryStringParameters"]
+    params: dict = event["queryStringParameters"]
+
+    inc = params["inc"] if "inc" in params.keys() and params["inc"] else 10
 
     filters = [{'Name': 'status', 'Values': ['in-use']}]
 
@@ -17,33 +51,82 @@ def handler(event, context):
         if "volume_id" in params.keys() and params["volume_id"]:
             filters.append({'Name': 'volume-id', 'Values': [params["volume_id"]]})
 
-    # Get all volumes
     print('[filters]', filters)
-    result = ec2.describe_volumes(Filters=filters)
-    print("available volumes")
-    print(result)
+
+    # Get all volumes
+    try:
+        result = ec2.describe_volumes(Filters=filters)
+        print("available volumes")
+        print(result)
+    except Exception as e:
+        print(f'[error] Unable to filter volumes. Exception: {e}')
+        return {
+            'statusCode': 400,
+            'headers': {'Content-Type': 'text/plain'},
+            'body': f"Unable to filter volumes. Exception: {e}"
+        }
 
     for volume in result['Volumes']:
-        print(f"Backing up {volume['VolumeId']} in {volume['AvailabilityZone']}")
+        volume_id = volume['VolumeId']
+        instance_id = volume['Attachments'][0]['InstanceId']
+
+        print(f"Backing up {volume_id} in {volume['AvailabilityZone']}")
 
         # Create snapshot
-        res = ec2.create_snapshot(VolumeId=volume['VolumeId'], Description='Created by backup_ebs lambda function')
+        try:
+            res = ec2.create_snapshot(VolumeId=volume_id, Description='Created by backup_ebs lambda function')
+        except Exception as e:
+            print(f'[error] Unable to create snapshot. Exception: {e}')
+            return {
+                'statusCode': 400,
+                'headers': {'Content-Type': 'text/plain'},
+                'body': f"Unable to create snapshot. Exception: {e}"
+            }
 
         # Get snapshot resource
-        ec2resource = boto3.resource('ec2')
-        snapshot = ec2resource.Snapshot(res['SnapshotId'])
+        try:
+            ec2resource = boto3.resource('ec2')
+            snapshot = ec2resource.Snapshot(res['SnapshotId'])
 
-        volume_name = ''
+            volume_name = ''
 
-        # Find name tag for volume
-        if 'Tags' in volume:
-            for tags in volume['Tags']:
-                if tags["Key"] == 'Name':
-                    volume_name = tags["Value"]
+            # Find name tag for volume
+            if 'Tags' in volume:
+                for tags in volume['Tags']:
+                    if tags["Key"] == 'Name':
+                        volume_name = tags["Value"]
+            else:
+                volume_name = f"Instance: {instance_id}"
+
+            # Add volume name to snapshot for easier identification
+            snapshot.create_tags(Tags=[{'Key': 'Name', 'Value': volume_name}])
+
+        except Exception as e:
+            print(f'[error] Unable to process snapshot. Exception: {e}')
+            return {
+                'statusCode': 400,
+                'headers': {'Content-Type': 'text/plain'},
+                'body': f"Unable to process snapshot. Exception: {e}"
+            }
+
+        try:
+            volume_size = get_ebs_volume_size(ec2, volume_id)
+        except Exception as e:
+            print(f'[error] Unable to get volume size. Exception: {e}')
+            return {
+                'statusCode': 400,
+                'headers': {'Content-Type': 'text/plain'},
+                'body': f"Unable to get volume size. Exception: {e}"
+            }
+
+        new_volume_size = int(volume_size * (1 + inc / 100))
+        print('[new volume size]', new_volume_size)
+
+        extended = extend_volume(ec2, volume_id, new_volume_size)
+
+        if extended["success"]:
+            wait_volume_modified(ec2, volume_id)
         else:
-            volume_name = f"Instance: {volume['Attachments'][0]['InstanceId']}"
-
-        # Add volume name to snapshot for easier identification
-        snapshot.create_tags(Tags=[{'Key': 'Name', 'Value': volume_name}])
+            return {'statusCode': 400, 'headers': {'Content-Type': 'text/plain'}, 'body': extended["message"]}
 
     return {'statusCode': 200, 'headers': {'Content-Type': 'text/plain'}, 'body': 'Snapshot created successfully'}
